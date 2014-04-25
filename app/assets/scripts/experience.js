c549304360b64b02f49e40ad0224bd9629e45fde
howard <howard.engelhart@outlook.com>
2014-04-25 17:29:46 -0400

    got experience working

--- app/assets/scripts/experience.js
+++ app/assets/scripts/experience.js
@@ -3,26 +3,78 @@
     'use strict';
 
     angular.module('c6.glickm')
-    .controller('ExperienceCtrl',['$log','$scope','experience',
-        function($log,$scope,experience){
+    .controller('ExperienceCtrl',['$log','$scope','$q',
+            'c6BrowserInfo','postMessage','experience',
+        function($log,$scope,$q,c6BrowserInfo,postMessage,experience){
         $log = $log.context('ExperienceCtrl');
         $log.info('instantiated with:', experience);
+        var self = this;
         $scope.experience = experience;
+
+        $scope.$on('iframeReady',function(event,$iframe){
+            self.registerExperience(experience,$iframe[0].contentWindow);
+        });
+
+        $scope.$on('$destroy',function(){
+            $log.info('got destroy, tear down experience and session');
+            self.deregisterExperience();
+        });
+
+        this.registerExperience = function(experience, expWindow) {
+            $log.info('EXPWINDOW:',expWindow);
+            var session = postMessage.createSession(expWindow);
+
+            $log.info('SESSION:',session);
+            session.experience = experience;
+            session.ready = false;
+
+            session.once('handshake', function(data, respond) {
+                $log.info('HAND SHAKE!');
+                respond({
+                    success: true,
+                    appData: {
+                        experience: experience,
+                        profile : c6BrowserInfo.profile
+                    }
+                });
+            });
+
+            session.once('ready', function() {
+                session.ready = true;
+                self.session = session;
+                $log.info('SESSION:',self.session);
+            });
+        };
+        
+        this.deregisterExperience = function() {
+            var session = self.session;
+            postMessage.destroySession(session.id);
+            delete self.session;
+        };
+
     }])
     .directive('c6Experience',['$log','c6UrlMaker',function($log,c6UrlMaker){
         $log = $log.context('c6Experience');
         function fnLink(scope,element){
-            $log.info('EXP:',scope.experience());
-            var url = c6UrlMaker(scope.experience().appUri,'exp');
-            $log.info('url:',url);
-            var $clone = angular.element('<div> get happy </div>');
-            element.append($clone);
+            var url = c6UrlMaker(
+                scope.experience.appUri + '/' +
+                scope.experience.appUriPrefix, 'exp');
+
+            $log.info('experience url:',url);
+            var $iframe = angular.element(
+                '<iframe src="' + url + '" width="100%" height="800"></iframe>'
+            );
+            $iframe.on('load',function(){
+                $log.info('experiences is loaded!');
+                scope.$emit('iframeReady',$iframe);
+            });
+            element.append($iframe);
         }
         return {
             link : fnLink,
             restrict : 'E',
             scope : {
-                experience : '&'
+                experience : '='
             }
         };
 
--- tasks/fixtures.js
+++ tasks/fixtures.js
@@ -3,11 +3,12 @@ module.exports = function(grunt) {
 
     grunt.registerTask('fixtures', 'Create a fixtures file based on plugin experience.json',
         function() {
-        var files = grunt.file.expand('app/assets/plugins/**/experiences.json');
+        var files = grunt.file.expand('app/assets/experiences/**/experiences.json');
         files.forEach(function(file){
             var exps = grunt.file.readJSON(file);
             exps.forEach(function(experience){
                 grunt.log.writelns('Write experience: ' + experience.id);
+                experience.appUriPrefix = 'app/index.html';
                 grunt.file.write('app/assets/mock/experiences/' +
                     experience.id + '.json',JSON.stringify(experience,null,3));
             });
